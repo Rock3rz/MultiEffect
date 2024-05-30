b05389335f1deccf91e0c1ee756a392010d06f92
PaolaPa1 <pavonepaola2003@gmail.com>
2024-05-30 14:57:53 +0200

    Look&Feel V 2.2

--- Source/MySlider.cpp
+++ Source/MySlider.cpp
@@ -11,6 +11,8 @@
 #include <JuceHeader.h>
 #include "MySlider.h"
 
+#define MARGIN 9.0f
+
 //==============================================================================
 MySlider::MySlider()
 {
@@ -20,7 +22,7 @@ MySlider::MySlider()
 void MySlider::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle,
     float rotaryEndAngle, juce::Slider& slider) {
 
-    auto radius = (float)juce::jmin(width / 2, height / 2) - 4.0f;
+    auto radius = (float)juce::jmin(width / 2, height / 2) - MARGIN;
     auto centreX = (float)x + (float)width * 0.5f;
     auto centreY = (float)y + (float)height * 0.5f;
     auto rx = centreX - radius;
@@ -51,14 +53,13 @@ void MySlider::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int 
     float dashLenghts[2] = {0.1f, 6.0f};
     dashedStroke.createDashedStroke(dashedArc, dashedArc, dashLenghts, 2);
     g.setColour(juce::Colours::darkgrey);
-    g.strokePath(dashedArc, dashedStroke);
+    g.strokePath(dashedArc, dashedStroke);  
 
     // Arc
     juce::Path valueArc;
     juce::PathStrokeType stroke(4.0f);
     stroke.setEndStyle(juce::PathStrokeType::EndCapStyle::butt);
-    valueArc.addCentredArc(centreX, centreY, radius+6, radius+6, 0.0f, rotaryStartAngle, angle, true);
+    valueArc.addCentredArc(centreX, centreY, radius + 6, radius + 6, 0.0f, rotaryStartAngle, angle, true);
     g.setColour(juce::Colours::mediumaquamarine);
-    g.strokePath(valueArc, stroke);   
-
+    g.strokePath(valueArc, stroke);
 }
